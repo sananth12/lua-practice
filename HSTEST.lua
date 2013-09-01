462858ac740c3131c1ccf7a12a2c553535df0847
sananth12 <sananthanatarajan12@gmail.com>
2013-09-01 23:52:35 +0530

    Codechef practice problems

---
+++ HSTEST.lua
@@ -0,0 +1,7 @@
+x=io.read("*number")
+y=io.read("*n")
+if(x%5==0 and y-x-0.5>=0) then
+	print (y-x-0.5)
+else
+	print(y)
+end
---
+++ TEST.lua
@@ -0,0 +1,7 @@
+while 1==1 do
+	i=io.read()
+	if(i=='42') then
+		break
+	end
+	print(i)
+end
